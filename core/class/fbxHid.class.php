14d7ae9a3b9089427e89c4d90e492457ec9209e3
Halbout Michael <michaelhalbout@gmail.com>
2017-07-27 15:10:33 +0200

    Add files via upload

---
+++ core/class/fbxHid.class.php
@@ -0,0 +1,105 @@
+<?php
+/* * ***************************Includes********************************* */
+require_once dirname(__FILE__) . '/../../../../core/php/core.inc.php';
+include_file('core', 'rudpconnexion', 'class', 'fbxHid');
+class fbxHid extends eqLogic {
+	public static $_widgetPossibility = array('custom' => array(
+	        'visibility' => true,
+	        'displayName' => true,
+	        'displayObjectName' => true,
+	        'optionalParameters' => true,
+	        'background-color' => true,
+	        'text-color' => true,
+	        'border' => true,
+	        'border-radius' => true
+	));
+	public function AddCommande($Name,$_logicalId,$Page,$Code) {
+		$Commande = $this->getCmd(null,$_logicalId);
+		if (!is_object($Commande))
+		{
+			$Commande = new fbxHidCmd();
+			$Commande->setId(null);
+			$Commande->setLogicalId($_logicalId);
+			$Commande->setEqLogic_id($this->getId());
+			$Commande->setType('action');
+			$Commande->setSubType('other');
+			$Commande->setName($Name);
+		}
+		$Commande->setConfiguration('Page',$Page);
+		$Commande->setConfiguration('Code',$Code);
+		$Commande->save();
+		return $Commande;
+	}
+	public function toHtml($_version = 'mobile') {
+		$replace = $this->preToHtml($_version);
+		if (!is_array($replace)) {
+			return $replace;
+		}
+		$version = jeedom::versionAlias($_version);
+		if ($this->getDisplay('hideOn' . $version) == 1) {
+			return '';
+		}
+		foreach ($this->getCmd(null, null, true) as $cmd) {
+			if($cmd->getIsVisible())	
+				$masque[]=$cmd->getLogicalId();
+			$replace['#'.$cmd->getLogicalId().'#'] = $cmd->toHtml($_version);
+		}
+
+		return $this->postToHtml($_version, template_replace($replace, getTemplate('core', $version, 'telecommande', 'fbxHid')));
+	}
+	public function postSave() {	
+		$this->AddCommande('Power','Power',0xc,0x30);
+		$this->AddCommande('AV','VCR/TV',0xc,0x63);	 
+		$this->AddCommande('0','Keypad0',0x7,0x59);	 	 
+		$this->AddCommande('1','Keypad1',0x7,0x5A);	 	 
+		$this->AddCommande('2','Keypad2',0x7,0x5B);	  
+		$this->AddCommande('3','Keypad3',0x7,0x5C);	 	 
+		$this->AddCommande('4','Keypad4',0x7,0x5D);	 	 
+		$this->AddCommande('5','Keypad5',0x7,0x5E);	 	 
+		$this->AddCommande('6','Keypad6',0x7,0x5F);	 	 
+		$this->AddCommande('7','Keypad7',0x7,0x60);	 	 
+		$this->AddCommande('8','Keypad8',0x7,0x61);	 	 
+		$this->AddCommande('9','Keypad9',0x7,0x62);	  
+		$this->AddCommande('Up','Up',0x7,0x4F);	  
+		$this->AddCommande('Down','Down',0x7,0x50);	  
+		$this->AddCommande('Left','Left',0x7,0x51);	  
+		$this->AddCommande('Right','Right',0x7,0x52);	 	 
+		$this->AddCommande('OK','Enter',0x7,0x28);	 
+		$this->AddCommande('Back','Exit',0xc,0x204);	
+		$this->AddCommande('Search','Search',0xc,0x221);	 
+		$this->AddCommande('Menu','Menu',0x1,0x86);		 
+		$this->AddCommande('Info','Properties',0xc,0x209);		 
+		$this->AddCommande('Free','manager',0xc,0x18f);		 
+		$this->AddCommande('Vol +','VolInc',0xc,0xE9);		
+		$this->AddCommande('Vol -','VolDec',0xc,0xEA);		 
+		$this->AddCommande('Mute','Mute',0xc,0xe2);	 
+		$this->AddCommande('Record','Record',0xc,0xb2);	 
+		$this->AddCommande('Prog +','ChannelInc',0xc,0x9c);	 
+		$this->AddCommande('Prog -','ChannelDec',0xc,0x9d);	 
+		$this->AddCommande('Rewind','Rewind',0xc,0xb4);	 
+		$this->AddCommande('Play/Pause','Play/Pause',0xc,0xcd);	 
+		$this->AddCommande('Fast Forward','Fast Forward',0xc,0xb3);	 
+		$this->AddCommande('Backspace','Backspace',0x7,0x2a);	 
+		$this->AddCommande('Stop','Stop',0xc,0xb7);	 
+		$this->AddCommande('Play','Play',0xc,0xb0);	 
+		$this->AddCommande('Random/Shuffle','Random play',0xc,0xb9);	 
+		$this->AddCommande('Next','Scan_Next_Track',0xc,0xb5);	 
+		$this->AddCommande('Prev','Scan_Previous_Track',0xc,0xb6);	 
+		$this->AddCommande('Zoom IN','Zoom In',0xc,0x22d); 
+		$this->AddCommande('Zoom OUT','Zoom Out',0xc,0x22e);	 
+		$this->AddCommande('Browser Back','Back',0xc,0x224);	 
+		$this->AddCommande('Browser Formward','Forward',0xc,0x225);	 
+		$this->AddCommande('Browser Refresh','Refresh',0xc,0x227);	 
+		$this->AddCommande('Browser Stop','Stop',0xc,0x226); 
+		$this->AddCommande('Next video track','Sub_Channel_Increment',0xc,0x171);
+		$this->AddCommande('Next audio track','Alt_Audio_Increment',0xc,0x173);
+		$this->AddCommande('Next subtitle track','Alt_Subtitle_Increment',0xc,0x175);
+		$this->AddCommande('Hibernate','System Sleep',0x1,0x82);	 
+		$this->AddCommande('Wake-up','System Wakeup',0x1,0x83);
+	}
+}
+class fbxHidCmd extends cmd {
+	public function execute($_options = array())	{
+		
+	}
+}
---
+++ core/class/rudpconnexion.class.php
@@ -0,0 +1,189 @@
+<?php
+class rudpConnection(){
+	private $testdescriptor= "10010000bd990000000000020000000054c3a96cc3a9636f6d6d616e6465204672656554c3a96c65630000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001000070005000c0000000c00000050c0901a10185019501751019002a8c021500268c0280c005010906a1018502050795017508150026ff00050719002aff0080c005010a8000a1018503750195041a81002a84008102750195048101c00000000000000000";
+	private $testhandshake = "02010000da59000000000000";
+	private $FOILS_COMMANDS = { 0 : 'FOILS_HID_DEVICE_NEW', # client to server
+		1 : 'FOILS_HID_DEVICE_DROPPED', # client to server
+		2 : 'FOILS_HID_DEVICE_CREATED', # server to client
+		3 : 'FOILS_HID_DEVICE_CLOSE', # server to client
+		4 : 'FOILS_HID_FEATURE', # bidir
+		5 : 'FOILS_HID_DATA', # bidir
+		6 : 'FOILS_HID_GRAB', # server to client
+		7 : 'FOILS_HID_RELEASE', # server to client
+		8 : 'FOILS_HID_FEATURE_SOLLICIT' } ;# server to client
+	private $COMMANDS = { 0 : 'RUDP_CMD_NOOP',
+		1 : 'RUDP_CMD_CLOSE',
+		2 : 'RUDP_CMD_CONN_REQ',
+		3 : 'RUDP_CMD_CONN_RSP',
+		4 : 'RUDP_CMD_PING',
+		5 : 'RUDP_CMD_PONG' };
+	private $FLAG = {
+		0: 'None',
+		1 : 'ACK',
+		2 : 'RELIABLE',
+		3 : 'ACK and RELIABLE',
+		4 : 'RETRANSMITTED', 
+		5 : 'RETRANSMITTED And ACK',
+		7 : 'RELIABLE AND RETRANSMITTED' ,
+		8 : 'UNRELIABLE',
+		9 : 'UNRELIABLE And ACK',
+		11 : 'UNRELIABLE AND RETRNASMITTED',
+	};
+	private $END_WAIT  = 10   ;   #Close Connection
+	private $SDR_PORT  = 50007;
+	private $RCV_PORT  = 50008;    # 50000-50010   
+	public $SYN       = 0x40000000;
+	private $ACK       = 0x20000000;
+	private $FIN       = 0x10000000;
+	private $DAT       = 0x08000000;
+	private $SYN_ACK   = 0x04000000;
+	private $ACK_DAT   = 0x02000000;
+	private $FIN_ACK   = 0x01000000;
+	private $MAX_PKTID = 0xffffff; 
+    public function __construct($destAddr, $isClient){
+        $this->destAddr = $destAddr;
+        $this->wait     = $this->SYN_ACK;
+		//if $isClient else $this->SYN
+        $this->pktId    = 0;
+        if(!$isClient){ 
+            $this->accept = [$this->SYN]; #[$this->SYN, $this->DAT, $this->FIN]
+            $this->time   = 0;
+            $this->data   = '';
+		}
+	}
+    public function checkTime($time){
+            if($time - $this->time) > $this->END_WAIT{
+                return False;
+			}
+            return True;
+		}
+	public function rudpPacket($pktType = null, $pktId = null, $data = ''){
+		return {'pktType': $pktType, 'pktId': $pktId, 'data': $data};
+	}
+	public function processSYN($rudpPkt, $c){
+		if(array_search($this->SYN, $c->accept)){
+			if($c->wait == $this->SYN){ 
+				$c->accept += [$this->DAT, $this->FIN];
+			}
+			$c->pktId = $rudpPkt['pktId'] + 1;
+			$c->wait  = $this->DAT;
+			$c->time  = time();
+			return $this->rudpPacket($this->SYN_ACK, $c->pktId);
+		}
+		throw new Exception('processSYN', $rudpPkt)
+	}
+	public function processDAT($rudpPkt, $c){
+		if($this->DAT == $c->wait){
+			if($rudpPkt['pktId'] == $c->pktId){
+				if(array_search($this->SYN,$c->accept){ 
+					$c->accept->remove($this->SYN)
+				}
+				$c->pktId += 1;
+				$c->data  += $rudpPkt['data'];
+				$c->time   = time() ;
+				return $this->rudpPacket($this->ACK, $c->pktId);
+			}elseif($rudpPkt['pktId'] == $c->pktId - 1){ 
+				$c->time   = time();
+				return $this->rudpPacket($this->ACK, $c->pktId);
+			}elseif $rudpPkt['pktId'] < $c->pktId - 1{ 
+				throw new Exception('processDAT [Duplicated]', $rudpPkt) 
+			}# Bugs
+		}
+		throw new Exception('processDAT', $rudpPkt);
+	}
+	public function processFIN($rudpPkt, $c){
+		if(array_search($this->FIN, $c->accept) and $rudpPkt['pktId'] == $c->pktId){
+			if(array_search($this->DAT, $c->accept)){ 
+				$c->accept->remove($this->DAT);
+			}
+			$c->wait = $this->FIN;
+			$c->time = time();
+			return $$this->rudpPacket($this->FIN_ACK, $c->pktId + 1);
+		}
+		throw new Exception('processFIN', $rudpPkt);
+	}
+	public function processSYN_ACK($rudpPkt, $c){
+		if($this->SYN_ACK == $c->wait and $rudpPkt['pktId'] == $c->pktId + 1){
+			$c->wait = $this->ACK;
+			$c->pktId += 1;
+			return $this->rudpPacket($this->DAT, $c->pktId)
+		}
+		throw new Exception('processSYN_ACK', $rudpPkt);
+	}
+	public function processACK($rudpPkt, $c){
+		if($this->ACK == $c->wait and $rudpPkt['pktId'] == $c->pktId + 1){
+			$c->pktId += 1;
+			return $this->rudpPacket($this->DAT, $c->pktId);
+		}
+		throw new Exception('processACK', $rudpPkt);
+	}
+	public function processFIN_ACK($rudpPkt, $c){
+		if(this->FIN_ACK == $c.wait and $rudpPkt['pktId'] == $c.pktId + 1){
+			$c.pktId += 1
+			throw new Exception( $c);
+		}
+		throw new Exception('processFIN_ACK', $rudpPkt);
+	}
+	public function encode($rudpPkt){ #pktId can be either ACK # or SEQ #
+		if($rudpPkt['pktId'] <= $MAX_PKTID){
+			$header = $rudpPkt['pktType'] | $rudpPkt['pktId'];
+			return pack('i', $header) + $rudpPkt['data'];
+		}
+		throw new Exception();
+	}
+	public function decode($bitStr){
+	    if (count($bitStr) < 4)
+		throw new Exception();
+		else{
+			$header  = unpack('i', $bitStr[4])[0]
+			return $this->rudpPacket($header & 0x7f000000, $header & 0x00ffffff, $bitStr[4]);
+		}
+	}
+}	
+class client(object){
+	private $MAX_DATA  = 1004;
+	private $MAX_RESND = 3;
+	private $RTO       = 3   ;    #The retransmission time period
+    public function __construct($address, $port){
+        $this->socketip=$address;
+        $this->socketport=$port;
+        $this->skt = socket_create(AF_INET, SOCK_DGRAM, 0); #UDP
+        
+        $this->skt->connect($address,$port);
+        #$this->skt->bind(('', $srcPort)); #used for recv
+    }
+    public function connect(){
+        $this->conn = new rudpConnection($this->socketip, $this->socketport);
+        socket_set_block($this->skt);
+        #$this->skt->settimeout(RTO)
+        while($this->MAX_RESND == $i){
+            try{ 
+				$data=$this->conn->encode($this->conn->rudpPacket($this->SYN, $pktId));
+				socket_sendto($this->skt,$data,strlen($data),0,$this->socketip);
+                //print $this->rudpPacket(SYN, $this->conn->pktId)     ## For debugging
+             
+                while(True){
+					$from = '';
+					$port = 0;
+					socket_recvfrom($this->skt, $recvData, $this->MAX_DATA, 0, $from, $port);
+                    try{ 
+                        $recvPkt = $this->conn->decode($recvData);
+                        //$sendPkt = rudpProcessSwitch[$recvPkt['pktType']]($recvPkt, $this->conn);
+                        return True;
+					}catch(){
+						continue;
+					}
+				}
+			}
+            catch timeout{ continue;}
+            catch Exception as e { 
+               // print e->message
+                //print '[Handshaking] unexpected error occurs\n' ## For debugging
+                return False;
+			}
+			$i++;
+		}
+		throw new Exception();
+	}
+}
+?>
