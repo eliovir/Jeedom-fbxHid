769515a601ea712a99dc21fc2d2fc8ae4b9f634d
Halbout Michael <michaelhalbout@gmail.com>
2017-07-27 15:29:51 +0200

    Update rudpconnexion.class.php

--- core/class/rudpconnexion.class.php
+++ core/class/rudpconnexion.class.php
@@ -2,35 +2,35 @@
 class rudpConnection{
 	private $testdescriptor= "10010000bd990000000000020000000054c3a96cc3a9636f6d6d616e6465204672656554c3a96c65630000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001000070005000c0000000c00000050c0901a10185019501751019002a8c021500268c0280c005010906a1018502050795017508150026ff00050719002aff0080c005010a8000a1018503750195041a81002a84008102750195048101c00000000000000000";
 	private $testhandshake = "02010000da59000000000000";
-	private $FOILS_COMMANDS = [ 
-		0 : 'FOILS_HID_DEVICE_NEW', # client to server
-		1 : 'FOILS_HID_DEVICE_DROPPED', # client to server
-		2 : 'FOILS_HID_DEVICE_CREATED', # server to client
-		3 : 'FOILS_HID_DEVICE_CLOSE', # server to client
-		4 : 'FOILS_HID_FEATURE', # bidir
-		5 : 'FOILS_HID_DATA', # bidir
-		6 : 'FOILS_HID_GRAB', # server to client
-		7 : 'FOILS_HID_RELEASE', # server to client
-		8 : 'FOILS_HID_FEATURE_SOLLICIT' ] ;# server to client
-	private $COMMANDS = [ 
-		0 : 'RUDP_CMD_NOOP',
-		1 : 'RUDP_CMD_CLOSE',
-		2 : 'RUDP_CMD_CONN_REQ',
-		3 : 'RUDP_CMD_CONN_RSP',
-		4 : 'RUDP_CMD_PING',
-		5 : 'RUDP_CMD_PONG' ];
-	private $FLAG = [
-		0: 'None',
-		1 : 'ACK',
-		2 : 'RELIABLE',
-		3 : 'ACK and RELIABLE',
-		4 : 'RETRANSMITTED', 
-		5 : 'RETRANSMITTED And ACK',
-		7 : 'RELIABLE AND RETRANSMITTED' ,
-		8 : 'UNRELIABLE',
-		9 : 'UNRELIABLE And ACK',
-		11 : 'UNRELIABLE AND RETRNASMITTED'
-	];
+	private $FOILS_COMMANDS = array( 
+		0 => 'FOILS_HID_DEVICE_NEW', # client to server
+		1 => 'FOILS_HID_DEVICE_DROPPED', # client to server
+		2 => 'FOILS_HID_DEVICE_CREATED', # server to client
+		3 => 'FOILS_HID_DEVICE_CLOSE', # server to client
+		4 => 'FOILS_HID_FEATURE', # bidir
+		5 => 'FOILS_HID_DATA', # bidir
+		6 => 'FOILS_HID_GRAB', # server to client
+		7 => 'FOILS_HID_RELEASE', # server to client
+		8 => 'FOILS_HID_FEATURE_SOLLICIT' ) ;# server to client
+	private $COMMANDS = array( 
+		0 => 'RUDP_CMD_NOOP',
+		1 => 'RUDP_CMD_CLOSE',
+		2 => 'RUDP_CMD_CONN_REQ',
+		3 => 'RUDP_CMD_CONN_RSP',
+		4 => 'RUDP_CMD_PING',
+		5 => 'RUDP_CMD_PONG' );
+	private $FLAG = array(
+		0 => 'None',
+		1 => 'ACK',
+		2 => 'RELIABLE',
+		3 => 'ACK and RELIABLE',
+		4 => 'RETRANSMITTED', 
+		5 => 'RETRANSMITTED And ACK',
+		7 => 'RELIABLE AND RETRANSMITTED' ,
+		8 => 'UNRELIABLE',
+		9 => 'UNRELIABLE And ACK',
+		11 => 'UNRELIABLE AND RETRNASMITTED'
+	);
 	private $END_WAIT  = 10   ;   #Close Connection
 	private $SDR_PORT  = 50007;
 	private $RCV_PORT  = 50008;    # 50000-50010   
