d657ef0b90556e5437b818f988bcac1567b481af
Halbout Michael <michaelhalbout@gmail.com>
2017-09-18 15:43:37 +0200

    Update cmdFbx.py

--- core/remotefreebox/cmdFbx.py
+++ core/remotefreebox/cmdFbx.py
@@ -1,7 +1,10 @@
 #! /usr/bin/env python3
 # coding: utf-8
-from freeboxcontroller import FreeboxController
+from .freeboxcontroller import FreeboxController
 import sys
 
 __main__
     fbx = FreeboxController()
+    fbx.press(sys.argv[0])
+    pass
+
